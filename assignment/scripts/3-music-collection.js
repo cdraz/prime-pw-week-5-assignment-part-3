7feb1a327ed6e5ef27da1c007caac26ddcb1bbf7
Christopher Razidlo <cd.razidlo@gmail.com>
2021-11-27 13:47:04 -0600

    added track list to collection, updated functions for track list

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,31 +1,35 @@
 console.log('***** Music Collection *****')
 let collection = []; // declaring collection variable
 
-// Adding addToCollection function
-const addToCollection = (title, artist, yearPublished) => {
+// Adding addToCollection function (Including tracks)
+const addToCollection = (title, artist, yearPublished, trackArray) => {
     let album = {
         title: title,
         artist: artist,
-        yearPublished: yearPublished
+        yearPublished: yearPublished,
+        tracks: trackArray
     }
     collection.push(album);
     return album;
 } // end addToCollection
 
-// testing addToCollection
-console.log(addToCollection('Punisher', 'Phoebe Bridgers', '2020'));
-console.log(addToCollection('Stranger in the Alps', 'Phoebe Bridgers', '2017'));
-console.log(addToCollection('The Money Store', 'Death Grips', '2012'));
-console.log(addToCollection('Helplessness Blues', 'Fleet Foxes', '2011'));
-console.log(addToCollection('Crack Up', 'Fleet Foxes', '2017'));
-console.log(addToCollection('How I\'m Feeling Now', 'Charli XCX', '2020'));
+// testing addToCollection (3 tracks per album because I'm lazy)
+console.log(addToCollection('Punisher', 'Phoebe Bridgers', '2020', [['DVD Menu', '1:09'], ['Garden Song', '3:39'], ['Kyoto', '3:04']]));
+console.log(addToCollection('Stranger in the Alps', 'Phoebe Bridgers', '2017', [['Smoke Signals', '5:24'], ['Motion Sickness', '3:49'], ['Funeral', '3:52']]));
+console.log(addToCollection('The Money Store', 'Death Grips', '2012', [['Get Got', '2:51'], ['The Fever (Aye Aye)', '3:06'], ['Lost Boys', '3:06']]));
+console.log(addToCollection('Helplessness Blues', 'Fleet Foxes', '2011', [['Montezuma', '3:36'], ['Bedouin Dress', '4:29'], ['Sim Sala Bim', '3:14']]));
+console.log(addToCollection('Crack Up', 'Fleet Foxes', '2017', [['I Am All That I Need / Arroyo Seco / Thumbprint Scar', '6:24'], ['Cassiues, -', '4:49'], ['- Naiads, Cassadies', '3:10']]));
+console.log(addToCollection('how i\'m feeling now', 'Charli XCX', '2020', [['pink diamond', '2:04'], ['forever', '4:03'], ['claws', '2:29']]));
 console.log(collection);
 
-// Adding showCollection function
+// Adding showCollection function (including tracks)
 const showCollection = array => {
     console.log(array.length);
     for (album of array) {
-        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+        console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}:`);
+        for (track of album.tracks) {
+            console.log(`${album.tracks.indexOf(track) + 1}. ${track[0]}: ${track[1]}`);
+        }
     }
 } // end showCollection
 
@@ -54,14 +58,20 @@ console.log(findByArtist('Charli XCX'));
 // Adding search function
 const search = searchCriteria => {
     let results = [];
-    if (searchCriteria === "" || searchCriteria === undefined) { // if blank search criteria or no criteria passed, return full collection
+    // if blank search criteria or no criteria passed, return full collection
+    if (searchCriteria === {} || searchCriteria === undefined) { 
         results = collection;
         return results;
     } 
     else {
     for (album of collection) {
-        if (searchCriteria.artist === album.artist && searchCriteria.year === album.yearPublished) { // search criteria is artist and year, both need to match to return result
-            results.push(album);
+        // *all* search criteria must match to return result - search format { artist: ARTIST, year: YEAR, trackName: TRACKNAME }
+        if (searchCriteria.artist === album.artist && searchCriteria.year === album.yearPublished) { 
+            // Now that we checked if the artist and year match, we loop through the tracks to make sure track name matches before we push result
+            for (track of album.tracks) {
+                if (searchCriteria.trackName === track[0])
+                results.push(album);
+            }
         }
      }
     }
@@ -70,6 +80,6 @@ const search = searchCriteria => {
 } // end search
 
 // testing search function
-console.log('Testing search: Phoebe Bridgers 2020 (expect Punisher match)', search({artist: 'Phoebe Bridgers', year: '2020'}));
-console.log('Testing search: Kacey Musgraves 2018 (expect empty array)', search({artist: 'Kacey Musgraves', year: '2018'}));
+console.log('Testing search: Phoebe Bridgers 2020 Kyoto (expect Punisher match)', search({artist: 'Phoebe Bridgers', year: '2020', trackName: 'Kyoto'}));
+console.log('Testing search: Kacey Musgraves 2018 Rainbow (expect empty array)', search({artist: 'Kacey Musgraves', year: '2018', trackName: 'Rainbow'}));
 console.log('Testing search: no search criteria (expecting full collection)', search());
