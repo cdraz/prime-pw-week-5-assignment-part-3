5a12b28ca23eb17014d11017017ee5415b8dd72f
Christopher Razidlo <cd.razidlo@gmail.com>
2021-11-27 11:20:40 -0600

    added addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,12 @@
 console.log('***** Music Collection *****')
-let collection = [];
+let collection = []; // declaring collection variable
+
+const addToCollection = (title, artist, yearPublished) => {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
